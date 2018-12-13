373e45faf2d5a6ab9fda37b0b3cc3d63536c09b0
Bryan Triana <bryantrianarueda@gmail.com>
2018-12-13 16:17:51 -0500

    Chatbot only responds to direct mentions

--- ChatBot/Chatbot.py
+++ ChatBot/Chatbot.py
@@ -1,11 +1,14 @@
 import os
 import time
+import re
 import slackclient
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
 socket_delay = 1
+direct_mention_regex = "^<@(|[WU].+?)>(.*)"
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 slack_client_id = os.environ["SLACK_CLIENT_ID"]
@@ -19,30 +22,38 @@ chatbot = ChatBot("Tycho")
 chatbot.set_trainer(ChatterBotCorpusTrainer)
 chatbot.train("chatterbot.corpus.english")
 
-def handleEvent(event):
-    user_id = event.get("user")
+def handle_event(event):
+    event_type = event.get("type")
     channel = event.get("channel")
     text = event.get("text")
-  
-    if user_id and channel and text:
-        handleMessage(channel, text)
 
-def handleMessage(channel, text):
-    response = str(chatbot.get_response(text))
+    if event_type == "message" and not "subtype" in event:
+        handle_message(channel, text)
+
+def handle_message(channel, text):
+    user_id, message = parse_direct_mention(text)
+
+    if user_id == bot_id:
+        response = str(chatbot.get_response(message))
     
-    sc.api_call("chat.postMessage", text=response, channel=channel)
+        sc.api_call("chat.postMessage", text=response, channel=channel)
 
-def runChatbot():
+def parse_direct_mention(text):
+    matches = re.search(direct_mention_regex, text)
+
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def run_chatbot():
     if sc.rtm_connect():
         while True:
             event_list = sc.rtm_read()
 
             for event in event_list:   
-               handleEvent(event)
+                handle_event(event)
            
             time.sleep(socket_delay)
        
 
 if __name__ == '__main__':
-    runChatbot()
+    run_chatbot()
     
