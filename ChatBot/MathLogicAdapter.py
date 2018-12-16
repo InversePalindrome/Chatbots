77bd18d4912bc0513b6ec36adf4e54e99eaeb80d
Bryan Triana <bryantrianarueda@gmail.com>
2018-12-15 21:04:50 -0500

    Separated importing of variables

--- ChatBot/MathLogicAdapter.py
+++ ChatBot/MathLogicAdapter.py
@@ -5,31 +5,51 @@ from chatterbot.logic import LogicAdapter
 from chatterbot.conversation import Statement
 
 
+symbol_table = cexprtk.Symbol_Table({}, {}, True)
+
+def evaluate_expression(expression):
+    try:
+        evaluated_expression = cexprtk.Expression(expression, symbol_table)()
+    except cexprtk.ParseException:
+        return Statement("Expression couldn't be evaluated!")
+            
+    solution = Statement(str(evaluated_expression))
+    solution.confidence = 1
+        
+    return solution
+
+def import_symbols(expression):
+    symbols = expression.split(' ', 1)
+    symbol_type = symbols[0]
+    symbols_dict = symbols[1].strip()
+
+    try:
+        if symbol_type == "constants":
+            symbol_table._populateConstants(ast.literal_eval(symbols_dict), True)
+            return Statement("Constants imported.")
+        elif symbol_type == "variables":
+            symbol_table._populateVariables(ast.literal_eval(symbols_dict))
+            return Statement("Variables imported.")
+    except (SyntaxError, ValueError):
+        return Statement("Nothing could be imported.")    
+
 class MathLogicAdapter(LogicAdapter):
     def __init__(self, **kwargs):
         return super().__init__(**kwargs)
 
     def can_process(self, statement):
-        return len(statement.text) > 0
+        return (statement.text.startswith("Evaluate") and len(statement.text.split(' ', 1)) > 1) or (
+            statement.text.startswith("Import") and len(statement.text.split(' ', 2)) > 2)
 
     def process(self, statement):
-        formula = statement.text.split(",", 1)
-        
-        expression = formula[0]
-        symbol_table = {}
+        statement_text = statement.text.split(' ', 1)
 
-        if len(formula) > 1:
-            try:
-                symbol_table = ast.literal_eval(formula[1].strip())
-            except(ValueError, SyntaxError):
-                return Statement("Symbol-Table couldn't be parsed!")
+        command = statement_text[0]
+        expression = statement_text[1]
 
-        try:
-            evaluaded_expression = cexprtk.evaluate_expression(expression, symbol_table)
-        except cexprtk.ParseException:
-            return Statement("Expression couldn't be evaluated!")
-            
-        solution = Statement(str(evaluaded_expression))
-        solution.confidence = 1
-        
-        return solution
+        if command == "Evaluate":
+            return evaluate_expression(expression)
+        elif command == "Import":
+            return import_symbols(expression)
+        else:
+            return Statement("Can't comprehend expression")
